c23240d305c62f11775edea88427bffe92b77ba8
ANtibushitse <ntibushitse.arsene@gmail.com>
2016-09-21 22:47:03 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// configurer son espace de travil avec javascrip 
+// Le .js peux s'écrire  dans plusieurs endroit; 
+// Les commentaire s'écrivent comme ceci
+// Chaque instructions doivent se terminer par un ; 
+// exemple: alet(Hello Word);
+// Le nom  des  variables  commences toujours par une majuscule  var lundi ≠ var Lundi
+var x = 24;
+var X = 42;
+// On peut déclarer plusiers variable en une fois  comme ci dessi 
+var nom = "Ntibushitse"
+	, prenom = 'Arsène'
+	, age = 24;
+var ville = "Toulouse";
+var dpt = "LRMP";
