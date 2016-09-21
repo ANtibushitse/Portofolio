fc21ed0e0e8e55516b43e964b94772677e9fcec1
ANtibushitse <ntibushitse.arsene@gmail.com>
2016-09-22 00:58:22 +0200

    ns cette version j'ai modifier  quelques commentaires dans le fichier .js

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 // Le .js peux s'écrire  dans plusieurs endroit : Dans le .html au travers de : <script> </script>  et dans un fichier extene comme celui ci. 
 //
 // Les commentaires s'écrivent comme ceci //
-// --- --- --- ---  Les valeurs -- -- -- --  Les types --- --- --- 
-//Une valeur est un morceau d'information utilisé dans un programme informatique. Les valeurs existent sous différentes formes, appelées des types. Le type d'une valeur  détermine l'oppération qu'il lui est affecté c'est à dire : 
-// ---- ------ ---- ---- ---- ---- ----  ---- Syntaxe . js --- --- --- --- --- --- --- --- --- --- --- -- 
-// Chaque instructions doivent se terminer par un ; 
-// exemple: alet(Hello Word);
-// Le nom  des  variables  commences toujours par une majuscule  var lundi:  ≠ var Lundi; 
-//--- --- --- -- -- -- -- -- -- -- - -- 
+/*Les commentaires multilignes comme cela */
+/* --- --- --- ---  Les valeurs -- -- -- --  Les types --- --- --- 
+/*Une valeur est un morceau d'information utilisé dans un programme informatique. Les valeurs existent sous différentes formes, appelées des types. Le type d'une valeur  détermine l'oppération qu'il lui est affecté c'est à dire. */
+/* ---- ------ ---- ---- ---- ---- ----  ---- Syntaxe . js --- --- --- --- --- --- --- --- --- --- --- -- 
+ Chaque instructions doivent se terminer par un ; 
+exemple: alet(Hello Word);
+ Le nom  des  variables  commences toujours par une majuscule  var lundi:  ≠ var Lundi; 
+*/
 var x = 24;
 var X = 42
 	, y = 108;
@@ -23,4 +24,4 @@ console.log(age);
 console.log(prenom);
 console.log(X / y);
 // -- -- -- -- -- -- -- -- -- --  Alors qu'est ce qu'une variable -- -- -- -- -- -- -- -- 
-// Une variable est un espace de stockage  dans laquelle on place des types ( vois au dessu )
+// Une variable est un espace de stockage  dans laqu
